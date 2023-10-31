099af67251cc427e01baf72e3a55e924dd4a80ad
Ozioma Emerem <code.emerem@gmail.com>
2023-10-31 23:41:35 +0100

    APIs work, Content management in progress

--- scheduler.py
+++ scheduler.py
@@ -2,41 +2,60 @@ import schedule
 import time
 
 # Define functions for each item
+
 def automation():
+    """Execute tasks related to Business Improvement: Automation"""
     print("Business Improvement: Automation")
 
 def data_engineering():
+    """Execute tasks related to Data Engineering: Democratized Insights"""
     print("Data Engineering: Democratized Insights")
 
 def tech_innovations():
+    """Execute tasks related to Tech Innovations: Robotics & AI"""
     print("Tech Innovations: Robotics & AI")
 
 def acquisitions():
+    """Execute tasks related to Business Growth Acquisitions: Diversification Strategy"""
     print("Business Growth Acquisitions: Diversification Strategy")
 
 def business_sales():
+    """Execute tasks related to Business Sales: Strategic Partnerships"""
     print("Business Sales: Strategic Partnerships")
 
 def personal_growth():
+    """Execute tasks related to Personal Growth: Self-development"""
     print("Personal Growth: Self-development")
 
 def life_experiences():
+    """Execute tasks related to Life experiences: Adventures"""
     print("Life experiences: Adventures")
 
 def affluence():
+    """Execute tasks related to Affluence: Wealth"""
     print("Affluence: Wealth")
 
 def stupendous_wealth():
+    """Execute tasks related to Stupendous Wealth: Opulence"""
     print("Stupendous Wealth: Opulence")
 
 def lifestyle():
+    """Execute tasks related to Lifestyle: Luxurious Living"""
     print("Lifestyle: Luxurious Living")
 
 # Function to schedule a job at a specified interval
+
 def schedule_job(job_func, interval):
+    """
+    Schedule a job function to run at a specified time interval.
+
+    :param job_func: The function to be executed.
+    :param interval: The time interval (in minutes) at which the job should be executed.
+    """
     schedule.every(interval).minutes.do(job_func)
 
 # Schedule each function separately
+
 schedule_job(automation, 5)
 schedule_job(data_engineering, 10)
 schedule_job(tech_innovations, 15)
@@ -49,6 +68,8 @@ schedule_job(stupendous_wealth, 45)
 schedule_job(lifestyle, 50)
 
 # Loop to keep the scheduler running
+
 while True:
     schedule.run_pending()
+    # Introduce a 1-second delay to prevent excessive CPU usage
     time.sleep(1)
